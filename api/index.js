e628a65a04e5238bd7b3697b251d1cc9e5574e00
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-12-28 15:52:50 +0530

    created mddleware

--- api/index.js
+++ api/index.js
@@ -5,10 +5,20 @@ import authRouter from "./routes/auth.route.js"
 mongoose.connect("mongodb+srv://sambhavdas:iamsdasbrain@cluster0.jqdos0l.mongodb.net/mern-estate?retryWrites=true&w=majority").then(()=>{console.log("connected to mongodb")}).catch((err)=>{
     console.log(err);
 })
-const app=express()
+const app=express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log("port listening on port 3000")
 })
 app.use('/api/user',userRouter)
 app.use('/api/auth', authRouter);
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode|| 501;
+    const message=err.message||'internal server error';
+return res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message,
+})
+})
 
